893fa09e268f7f9f9eab2f4bcb2453f61354c9b4
tukiminya <me@tuki.dev>
2024-09-05 18:02:57 +0900

    feat: Add header componets

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+import { LucideCircleOff } from "lucide-react"
+
+type HeaderProps = {
+  title: string
+}
+export function Header({title}: HeaderProps) {
+  return (
+    <header className="flex justify-between w-full py-4 px-8 items-center">
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <div className="flex gap-4">
+        <div className="bg-yellow-200 p-6 text-2xl font-bold">
+          音量が100%ではありません
+        </div>
+        <button className="aspect-square p-6 bg-red-700">
+          <LucideCircleOff className="text-white h-8 w-8" />
+        </button>
+        </div>
+      </header>
+  )
+}
