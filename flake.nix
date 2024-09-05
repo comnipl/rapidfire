438733ac5a5705edd714188735bb5feb5a3e5638
AsPulse / あすぱる <contact@aspulse.dev>
2024-09-05 17:31:08 +0900

    chore: Initial commit, tauri workspace setup

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "developing environment of Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { 
+	  inherit system;
+	};
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              libiconv
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.CoreServices
+              darwin.apple_sdk.frameworks.CoreFoundation
+              darwin.apple_sdk.frameworks.Foundation
+              darwin.apple_sdk.frameworks.AppKit
+              darwin.apple_sdk.frameworks.WebKit
+              darwin.apple_sdk.frameworks.Cocoa
+              minio-client
+              python312Packages.boto3
+              rustup
+              libsndfile
+            ];
+            shellHook = ''
+              exec $SHELL
+            '';
+          };
+        };
+      }
+    );
+}
