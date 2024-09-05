08dd10bc70119cdd1b169ef9fa2393f9542453d2
AsPulse / あすぱる <contact@aspulse.dev>
2024-09-05 18:18:12 +0900

    dbg current_dir

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +10,8 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    dbg!(env::current_dir().unwrap());
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
