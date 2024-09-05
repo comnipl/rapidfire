1c754bff7fbfaf69e5492acac633a002e52fb8a0
つきみ <me@tuki.dev>
2024-09-05 02:20:47 -0700

    feat(frontend): Add sidebar (#5)

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils"
+import { HTMLAttributes } from "react"
+
+const schene = [
+  {
+    title: "Rapidfire"
+  }
+]
+export function SideBar({ className}:HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={cn("w-full h-full bg-white px-4 flex flex-col gap-4", className)}>
+      {schene.map((item, i) => (
+        <button className="w-full h-16 bg-gray-200 text-2xl font-bold" key={i}>{item.title}</button>
+      ))}
+    </div>
+  )
+}
