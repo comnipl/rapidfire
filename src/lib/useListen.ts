c4f70d24e3e6f335068c58baa3a3c297f3380b4c
AsPulse / あすぱる <contact@aspulse.dev>
2024-09-15 01:07:24 +0000

    useListen only execute if in tauri

--- src/lib/useListen.ts
+++ src/lib/useListen.ts
@@ -1,16 +1,19 @@
 import { listen, type EventCallback } from "@tauri-apps/api/event";
 import { useEffect } from "react";
+import { isTauri } from "./isTauri";
 
 export const useListen = <T>(
     eventName: string,
     action: (event: Parameters<EventCallback<T>>[0]) => Promise<void>
 ) => {
     useEffect(() => {
-        const unlisten = listen<T>(eventName, payload => {
-            action(payload);
-        });
-        return () => {
-            unlisten.then((f) => f());
-        };
+        if (isTauri()) {
+            const unlisten = listen<T>(eventName, payload => {
+                action(payload);
+            });
+            return () => {
+                unlisten.then((f) => f());
+            };
+        }
     }, [eventName, action]);
 };
